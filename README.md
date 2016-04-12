a8d542bb5b34388cdf9c5bf92ee9c13e36d005fd
Ariana Giorgi <ariana@AGIORGIMac.local>
2016-04-12 12:23:42 -0400

    Create sample tweet output, update readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # reliable-sources
 Project to return a list of reliable on-the-scene sources from Twitter.
+
+## Set up and Installation ##
+Set up virtualenv
+
+Run `pip install -r requirements.txt`
+
+## Usage ##
+`python main.py <Location>`
+
+Example: `python main.py Toronto`
+
+## Twython ##
+List of interface options available [here](
+https://twython.readthedocs.org/en/latest/api.html)
--- collect.py
+++ collect.py
@@ -4,26 +4,40 @@ from twython import Twython
 from config import (consumer_key, consumer_secret, access_token, access_token_secret)
 
 def filter_sources(tweet):
+	### TK
+
 	pass
 
 def find_tweets(twitter, place_id):
+	### Return tweets from a certain place id
+
 	#`count` param defaults to 15, maximum of 100. using 3 for now to test
 	results = twitter.search(q = 'place:'+place_id, count = '3')
 
 	#turn results in json string
 	tweets = json.loads( json.dumps(results) )
 
+	print "Tweets:"
 	for i in range(len(tweets['statuses'])):
 		tweet_id = tweets['statuses'][i]['id_str']
 
-		#retrieve tweet based on tweet_id ... twitter.search or .lookup_status?
+		#retrieve tweet based on tweet_id
+		t = twitter.lookup_status(id = tweet_id)
+		tweet = json.loads( json.dumps(t) )
+
+		#sample parse for tweet output
+		print tweet[0]['user']['screen_name']+': '+tweet[0]['text']
 
 		#this is where we're going to filter these tweets then return
 		#filter_sources(tweet)
 
+		#pydrive?
+
 
 
 def find_place(location):
+	### Determine place id based on querying location
+
 	#configure twython connection
 	twitter = Twython(consumer_key, consumer_secret, access_token, access_token_secret)
 
@@ -37,6 +51,7 @@ def find_place(location):
 	print "Results based on your provided location:"
 	for i in range(len(locations['result']['places'])):
 		print str(i+1)+'. ', locations['result']['places'][i]['full_name']
+		### TODO: option for "other"
 
 	place_number = input('Please enter the location you want to use: ')
 	place_number = place_number - 1 #adjust for array counting
