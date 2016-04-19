bc41839f14ae435d3a1b11ec2baf17b194d2144b
SARA SIMON <smbsimon@gmail.com>
2016-04-19 14:08:53 -0500

    standardize comments, remove superfluous json dumps and loads

--- send.py
+++ send.py
@@ -5,14 +5,19 @@ from datetime import date, datetime
 from access import get_google_sheet
 
 def fetch_data(tweet):
-    username = tweet[0]['user']['screen_name']
-    tweet_content = tweet[0]['text']
-    timestamp = tweet[0]['created_at']
+    ### pull in and parse reliable tweets
 
-    #number of followers user has
+    #find relevant information about the tweeter
+    username = tweet[0]['user']['screen_name']
     followers = tweet[0]['user']['followers_count']
 
-    #check if there's an image included
+    #find relevant information about the tweet
+    tweet_content = tweet[0]['text']
+    timestamp = tweet[0]['created_at']
+    favorites = tweet[0]['favorite_count']
+    retweets = tweet[0]['retweet_count']
+
+    #check to see if there is an image included
     text = tweet[0]['text'].lower()
 
     if 't.co' in text:
@@ -20,30 +25,29 @@ def fetch_data(tweet):
     else:
 		image = 'No'
 
-	#favorites and retweets
-    favorites = tweet[0]['favorite_count']
-    retweets = tweet[0]['retweet_count']
-
     return username, tweet_content, timestamp, followers, image, favorites, retweets
 
+
 def send_to_google_drive(reliable_tweets):
+    ### send tweet and its relevant information to google drive
+
     #open google sheet
     sheet = get_google_sheet()
 
-    #set the title of the worksheet user's location input and keyword, if applicable
+    #prep worksheet title to include the user's location input and keyword, if applicable
     if sys.argv[2]:
         worksheet_name = sys.argv[1]+": "+sys.argv[2]
     else:
         worksheet_name = sys.argv[1]
 
-    #find and format the date to add to worksheet title
+    #find date and timestamp to be added to the worksheet title
     today = date.today()
     created_on = datetime.now().strftime('%m-%d-%y: %H:%M:%S')
 
     #create a new worksheet with a title of the location and date
     new_worksheet = sheet.add_worksheet(title=worksheet_name+": "+created_on, rows="101", cols="20")
 
-    #set headers
+    #set worksheet headers
     new_worksheet.update_cell(1, 1, 'User')
     new_worksheet.update_cell(1, 2, 'Tweet')
     new_worksheet.update_cell(1, 3, 'Timestamp')
